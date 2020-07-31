e1cdba90ab98b5cc218da71dfda9b8989312fbed
DalueWang <790409555@qq.com>
2020-07-31 10:21:59 -0400

    new updates on layout

--- my.js
+++ my.js
@@ -1,9 +1,11 @@
+/*
 document.getElementById("aff").style.height = document.getElementsByClassName("myphoto")[0].offsetHeight + "px";
 console.log("Hello...", document.getElementsByClassName("myphoto")[0].offsetHeight)
 window.onresize = changeSize
 function changeSize () {
     document.getElementById("aff").style.height = document.getElementsByClassName("myphoto")[0].offsetHeight + "px";
 }
+*/
 $(document).ready(function (){
     document.getElementById("BUT_INTRODUCTION_PART").onclick = function () {
         var X = $('#INTRODUCTION_PART').offset().top;
@@ -21,13 +23,4 @@ $(document).ready(function (){
         var X = $('#RESEARCH_PUB_PART').offset().top;
         window.scrollTo(0, X - 70); 
     }
-    /*
-    console.log("Hello...", document.getElementsByClassName("myphoto")[0].offsetHeight)
-    document.getElementById("aff").style.height = document.getElementsByClassName("myphoto")[0].offsetHeight + "px";
-    document.getElementById('photo').onload=function(){
-        // 加载完成
-        console.log("Hello", document.getElementById("photo").offsetHeight)
-        document.getElementById("aff").style.height = document.getElementById("photo").offsetHeight + "px";
-    };
-    */
 })
