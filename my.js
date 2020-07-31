6f1153d867132c4934877a7a617d8fdadbe683a2
DalueWang <790409555@qq.com>
2020-07-31 16:27:21 -0400

    new updates on scroll

--- my.js
+++ my.js
@@ -9,18 +9,18 @@ function changeSize () {
 $(document).ready(function (){
     document.getElementById("BUT_INTRODUCTION_PART").onclick = function () {
         var X = $('#INTRODUCTION_PART').offset().top;
-        window.scrollTo(0, X - 70); 
+        window.scrollTo(0, X - 3.5 * document.documentElement.clientWidth / 100); 
     }
     document.getElementById("BUT_EXPERIENCE_PART").onclick = function () {
         var X = $('#EXPERIENCE_PART').offset().top;
-        window.scrollTo(0, X - 70); 
+        window.scrollTo(0, X - 3.5 * document.documentElement.clientWidth / 100); 
     }
     document.getElementById("BUT_RESEARCH_INTERE_PART").onclick = function () {
         var X = $('#RESEARCH_INTERE_PART').offset().top;
-        window.scrollTo(0, X - 70); 
+        window.scrollTo(0, X - 3.5 * document.documentElement.clientWidth / 100); 
     }
     document.getElementById("BUT_RESEARCH_PUB_PART").onclick = function () {
         var X = $('#RESEARCH_PUB_PART').offset().top;
-        window.scrollTo(0, X - 70); 
+        window.scrollTo(0, X - 3.5 * document.documentElement.clientWidth / 100); 
     }
 })
